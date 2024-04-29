2785c6590aabc6258077972837330941ba0c89a7
skmutai6 <skmutai6@gmail.com>
2024-04-29 13:00:25 +0300

    code along

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,13 @@
 import React from "react";
 
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
+  function handleDeleteClick() {
+    fetch(`http://localhost:4000/items/${item.id}`, {
+    method: "DELETE",
+  })
+    .then((res) => res.json())
+    .then(() => onDeleteItem(item));
+  }
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
@@ -8,7 +15,7 @@ function Item({ item }) {
       <button className={item.isInCart ? "remove" : "add"}>
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
-      <button className="remove">Delete</button>
+      <button className="remove" onClick={handleDeleteClick} >Delete</button>
     </li>
   );
 }
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,18 +1,36 @@
 import React, { useState } from "react";
 
-function ItemForm() {
+function ItemForm({ onAddItem }) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    const itemData = {
+      name: name,
+      category: category,
+      isInCart: false,
+    };
+    fetch("http://localhost:4000/items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(itemData),
+    })
+      .then((r) => r.json())
+      .then((newItem) => onAddItem(newItem));
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
         <input
           type="text"
           name="name"
           value={name}
-          onChange={(e) => setName(e.target.value)}
+          onChange={(event) => setName(event.target.value)}
         />
       </label>
 
@@ -21,7 +39,7 @@ function ItemForm() {
         <select
           name="category"
           value={category}
-          onChange={(e) => setCategory(e.target.value)}
+          onChange={(event) => setCategory(event.target.value)}
         >
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
